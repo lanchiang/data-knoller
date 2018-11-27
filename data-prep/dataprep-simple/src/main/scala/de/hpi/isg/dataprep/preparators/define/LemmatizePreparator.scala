b9232253902b4faf768f49d6c0c1911539df179c
Daniel Theveßen <danthe96@gmx.de>
2018-11-27 14:15:11 +0100

    Update test

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/LemmatizePreparator.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/LemmatizePreparator.scala
@@ -3,9 +3,12 @@ package de.hpi.isg.dataprep.preparators.define
 import de.hpi.isg.dataprep.components.Preparator
 import de.hpi.isg.dataprep.exceptions.ParameterNotSpecifiedException
 import de.hpi.isg.dataprep.metadata.PropertyDataType
+import de.hpi.isg.dataprep.preparators.implementation.DefaultLemmatizePreparatorImpl
 import de.hpi.isg.dataprep.util.DataType
 
 class LemmatizePreparator(val propertyName : String) extends Preparator {
+
+  this.impl = new DefaultLemmatizePreparatorImpl
 
   /**
     * This method validates the input parameters of a {@link AbstractPreparator}. If succeeds, setup the values of metadata into both
--- data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/LemmatizeTest.java
+++ data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/LemmatizeTest.java
@@ -6,10 +6,12 @@ import de.hpi.isg.dataprep.model.repository.ErrorRepository;
 import de.hpi.isg.dataprep.model.target.errorlog.ErrorLog;
 import de.hpi.isg.dataprep.model.target.system.AbstractPreparation;
 import de.hpi.isg.dataprep.preparators.define.LemmatizePreparator;
+import org.apache.spark.sql.Row;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -18,15 +20,34 @@ import java.util.List;
 public class LemmatizeTest extends PreparatorTest {
 
     @Test
-    public void testAdjective() throws Exception {
-        // TODO: Test with dataset with adjectives like 'better', 'worst', etc.
-        Preparator preparator = new LemmatizePreparator("id");
+    public void testValidColumn() throws Exception {
+        Preparator preparator = new LemmatizePreparator("stemlemma");
 
         AbstractPreparation preparation = new Preparation(preparator);
         pipeline.addPreparation(preparation);
         pipeline.executePipeline();
 
         pipeline.getRawData().show();
+
+        List<ErrorLog> errorLogs = new ArrayList<>();
+        ErrorRepository errorRepository = new ErrorRepository(errorLogs);
+
+        Assert.assertEquals(errorRepository, pipeline.getErrorRepository());
+    }
+
+    @Test
+    public void testInValidColumn() throws Exception {
+        Preparator preparator = new LemmatizePreparator("stemlemma_wrong");
+
+        AbstractPreparation preparation = new Preparation(preparator);
+        pipeline.addPreparation(preparation);
+        pipeline.executePipeline();
+
+        pipeline.getRawData().show();
+//        String[] rows = new String[]{""};
+//        for(Iterator<Row> iter = pipeline.getRawData().toLocalIterator(), int i=0; iter.hasNext();i++){
+//            Row row = iter.next();
+//        }
 
         List<ErrorLog> errorLogs = new ArrayList<>();
         ErrorRepository errorRepository = new ErrorRepository(errorLogs);
