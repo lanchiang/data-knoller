38acc2230ea6c1a288604446a3625a328317c323
Daniel Theveßen <danthe96@gmx.de>
2018-12-01 19:24:13 +0100

    Reformat

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultLemmatizePreparatorImpl.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultLemmatizePreparatorImpl.scala
@@ -23,8 +23,8 @@ class DefaultLemmatizePreparatorImpl extends PreparatorImpl {
     * The abstract class of preparator implementation.
     *
     * @param abstractPreparator is the instance of { @link AbstractPreparator}. It needs to be converted to the corresponding subclass in the implementation body.
-    * @param dataFrame        contains the intermediate dataset
-    * @param errorAccumulator is the { @link CollectionAccumulator} to store preparation errors while executing the preparator.
+    * @param dataFrame          contains the intermediate dataset
+    * @param errorAccumulator   is the { @link CollectionAccumulator} to store preparation errors while executing the preparator.
     * @return an instance of { @link ExecutionContext} that includes the new dataset, and produced errors.
     * @throws Exception
     */
@@ -32,62 +32,62 @@ class DefaultLemmatizePreparatorImpl extends PreparatorImpl {
     val preparator = abstractPreparator.asInstanceOf[LemmatizePreparator]
     val propertyNames = preparator.propertyNames
 
-    var createdDataset : Dataset[Row] = dataFrame
+    var createdDataset: Dataset[Row] = dataFrame
 
-      val rowEncoder = RowEncoder(createdDataset.schema)
-      createdDataset = createdDataset.flatMap(row => {
-        //
-        def lemmatizeString(str: String): String = {
+    val rowEncoder = RowEncoder(createdDataset.schema)
+    createdDataset = createdDataset.flatMap(row => {
+      //
+      def lemmatizeString(str: String): String = {
 
-          val props = new Properties()
-          props.setProperty("annotators", "tokenize,ssplit,pos,lemma")
-          val pipeline = new StanfordCoreNLP(props)
-          val document = new Annotation(str)
-          pipeline.annotate(document)
+        val props = new Properties()
+        props.setProperty("annotators", "tokenize,ssplit,pos,lemma")
+        val pipeline = new StanfordCoreNLP(props)
+        val document = new Annotation(str)
+        pipeline.annotate(document)
 
-          val sentences = document.get(classOf[CoreAnnotations.SentencesAnnotation])
-          if (sentences.size() != 1)
-            throw new Exception("Field empty or more than one sentence supplied")
-          val tokens = sentences.get(0).get(classOf[CoreAnnotations.TokensAnnotation])
-          if (tokens.size() != 1)
-            throw new Exception("Field empty or more than one token supplied")
-          val lemmatized = tokens.get(0).get(classOf[CoreAnnotations.LemmaAnnotation])
+        val sentences = document.get(classOf[CoreAnnotations.SentencesAnnotation])
+        if (sentences.size() != 1)
+          throw new Exception("Field empty or more than one sentence supplied")
+        val tokens = sentences.get(0).get(classOf[CoreAnnotations.TokensAnnotation])
+        if (tokens.size() != 1)
+          throw new Exception("Field empty or more than one token supplied")
+        val lemmatized = tokens.get(0).get(classOf[CoreAnnotations.LemmaAnnotation])
 
-          lemmatized
+        lemmatized
+      }
+
+      val remappings = propertyNames.map(propertyName => {
+        val indexTry = Try {
+          row.fieldIndex(propertyName)
         }
+        val index = indexTry match {
+          case Failure(content) => throw content
+          case Success(content) => content
+        }
+        val operatedValue = row.getAs[String](index)
+        (index, operatedValue)
+      }).toMap
 
-        val remappings = propertyNames.map(propertyName => {
-          val indexTry = Try {
-            row.fieldIndex(propertyName)
-          }
-          val index = indexTry match {
-            case Failure(content) => throw content
-            case Success(content) => content
-          }
-          val operatedValue = row.getAs[String](index)
-          (index, operatedValue)
-        }).toMap
+      val seq = row.toSeq
+      val tryConvert = Try {
+        val newSeq = seq.zipWithIndex.map(tuple => {
+          if (remappings.isDefinedAt(tuple._2))
+            lemmatizeString(remappings.get(tuple._2).get)
+          tuple._1
+        })
+        val newRow = Row.fromSeq(newSeq)
+        newRow
+      }
 
-          val seq = row.toSeq
-          val tryConvert = Try {
-            val newSeq = seq.zipWithIndex.map(tuple => {
-              if(remappings.isDefinedAt(tuple._2))
-                lemmatizeString(remappings.get(tuple._2).get)
-              tuple._1
-            })
-            val newRow = Row.fromSeq(newSeq)
-            newRow
-          }
-
-          val trial = tryConvert match {
-            case Failure(content) => {
-              errorAccumulator.add(new RecordError(remappings.values.toString(), content))
-              tryConvert
-            }
-            case Success(content) => tryConvert
-          }
-          trial.toOption
-      })(rowEncoder)
+      val trial = tryConvert match {
+        case Failure(content) => {
+          errorAccumulator.add(new RecordError(remappings.values.toString(), content))
+          tryConvert
+        }
+        case Success(content) => tryConvert
+      }
+      trial.toOption
+    })(rowEncoder)
 
 
     new ExecutionContext(createdDataset, errorAccumulator)
