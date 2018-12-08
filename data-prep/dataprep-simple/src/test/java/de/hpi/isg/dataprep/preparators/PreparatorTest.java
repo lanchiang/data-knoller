4cd9b1e33920e61d26ba0ffb5436db4ff8bd9da7
LasLitz <L.Kohlmeyer@gmx.net>
2018-12-08 23:24:09 +0100

    Remove Preamble Pullrequest (#7)
    * Add files via upload
    
    Added Implemenation Class of Remove Preamble
    
    * Add files via upload
    
    Added Definition Class of Remove Preamble
    
    * Add files via upload
    
    Added own Testcases
    
    * Add files via upload
    
    Added awn test files and solutions
    
    * Add files via upload
    
    Added FileMetaData
    
    * Add files via upload
    
    Added new metatadata

--- data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/PreparatorTest.java
+++ data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/PreparatorTest.java
@@ -33,17 +33,35 @@ public class PreparatorTest {
     protected static Dataset<Row> dataset;
     protected static AbstractPipeline pipeline;
     protected static DataContext dataContext;
+    protected static FileLoadDialect dialect;
 
     @BeforeClass
     public static void setUp() {
         Logger.getLogger("org").setLevel(Level.OFF);
         Logger.getLogger("akka").setLevel(Level.OFF);
 
-        FileLoadDialect dialect = new DialectBuilder()
+        dialect = new DialectBuilder()
                 .hasHeader(true)
                 .inferSchema(true)
-                .url("./src/test/resources/restaurants.tsv")
-                .delimiter("\t")
+                //.delimiter("\t")
+                //x: works
+                //.url("./src/test/resources/pokemon.csv")//->X
+                //.url("./src/test/resources/restaurants.tsv")//->x
+               //.url("./src/test/resources/test.csv")//->x
+                //.url("./src/test/resources/test2.csv")//->x
+                //.url("./src/test/resources/test21.csv")//->x
+                // .url("./src/test/resources/test3.csv")//->x
+                //.url("./src/test/resources/test4.csv")//->x
+                //.url("./src/test/resources/test5.csv")//->x
+                .url("./src/test/resources/test6.csv")//->x
+                //.url("./src/test/resources/test7.csv")//->x
+                //.url("./src/test/resources/test8.csv")//-> x,
+                //.url("./src/test/resources/test9.csv")//->x
+                //.url("./src/test/resources/test10.csv")//->x
+                //.url("./src/test/resources/test11.csv")//->x
+
+//                 .url("./src/test/resources/restaurants.tsv")
+//                 .delimiter("\t")
                 .buildDialect();
 
 //        FileLoadDialect dialect = new DialectBuilder()
@@ -55,7 +73,6 @@ public class PreparatorTest {
 
         SparkDataLoader dataLoader = new FlatFileDataLoader(dialect);
         dataContext = dataLoader.load();
-
 //        dataContext.getDataFrame().show();
         return;
     }
