1c6b5c05f2723a3ce3a30ce10fbc41d3dc41c8f7
sirexeclp <sirexeclp@gmail.com>
2019-01-14 16:37:30 +0100

    implement calApplicability function

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/MergeAttribute.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/MergeAttribute.scala
@@ -13,14 +13,46 @@ class MergeAttribute (val attributes:List[String]
 
 	def this(attributes: java.util.List[String], connector:String)
 	{
-		this(attributes.asScala.toList,connector)
+		this((attributes.asScala.toList), connector)
+	}
+
+	//def this(attributes: java.util.List[Integer], connector: String)
+	//{
+	//	this(Right(attributes.asScala.toList), connector)
+	//}
+	def isNull(value:String) = value.trim.isEmpty
+
+	def isGoodToMerge(a:String, b:String):Integer = {
+		if (a.equals(b))
+			1
+			else if ( isNull(a) || isNull(b))
+			1
+		else
+			0
 	}
 
 
 	override def buildMetadataSetup(): Unit = {
 
 	}
-	override def calApplicability(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]): Float = 0
+	override def calApplicability(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]): Float = {
+		import dataset.sparkSession.implicits._
+		val columns = dataset.columns.toSeq
+
+		dataset.columns.flatMap (
+			col => (
+				col.zip(columns.filter(x => x != col))
+				)
+			)
+				.map(x => dataset.select(x._1.toString, x._2))
+				.map(colCombo =>
+					colCombo.map(
+						row => isGoodToMerge(row.getString(0), row.getString(1))
+					)
+							.reduce(_ + _)
+				).max / dataset.count()
+
+	}
 
 
 }
