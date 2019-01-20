21238fe042a55bad3ba4fe4a77bff386b4a5b2a9
Oliver Clasen <oliver.clasen@alice-dsl.net>
2019-01-20 13:47:25 +0100

    implemented calApplicability function

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/ChangeTableEncoding.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/ChangeTableEncoding.scala
@@ -1,10 +1,14 @@
 package de.hpi.isg.dataprep.preparators.define
 
+import java.io.File
+import java.nio.file.{Files, Paths}
 import java.util
 
+import de.hpi.isg.dataprep.metadata.CSVSourcePath
 import de.hpi.isg.dataprep.model.target.objects.Metadata
 import de.hpi.isg.dataprep.model.target.schema.SchemaMapping
 import de.hpi.isg.dataprep.model.target.system.AbstractPreparator
+import org.apache.directory.api.util.Unicode
 import org.apache.spark.sql.{Dataset, Row}
 
 /**
@@ -24,7 +28,52 @@ class ChangeTableEncoding(val propertyName: String,
     // TODO
   }
 
+  private def countErrorsInFile(): Int = {
+    val replacementChar = new Array[Byte](3)
+    replacementChar(0) = Byte(0xEF)
+    replacementChar(1) = Byte(0xBF)
+    replacementChar(0) = Byte(0xBD)
+
+    var errorCount = 0
+
+    val bytesRead = new Array[Byte](3)
+    bytesRead(0) = 0x00
+    bytesRead(1) = 0x00
+    bytesRead(2) = 0x00
+
+    val buf = new Array[Byte](1)
+    val inStream = Files.newInputStream(Paths.get(this.getPreparation.getPipeline.getMetadataRepository.getMetadata(CSVSourcePath).toString))
+
+    var byteRead = inStream.read(buf)
+    while (byteRead > 0) {
+      bytesRead(0) = bytesRead(1)
+      bytesRead(1) = bytesRead(2)
+      bytesRead(2) = Byte(byteRead)
+      if (bytesRead.equals(replacementChar)) {
+        errorCount += 1
+      }
+      byteRead = inStream.read(buf)
+    }
+    errorCount
+  }
+
   override def calApplicability(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]): Float = {
-    0
+    val replacementChar = new Array[Byte](3)
+    replacementChar(0) = Byte(0xEF)
+    replacementChar(1) = Byte(0xBF)
+    replacementChar(0) = Byte(0xBD)
+    var errorCounter = 0
+
+    val csvFile = new File(this.getPreparation.getPipeline.getMetadataRepository.getMetadata(CSVSourcePath).toString)
+
+    if (dataset != this.getPreparation.getPipeline.getRawData) {
+      return 0
+    }
+
+    dataset.foreach(row => {
+      errorCounter = errorCounter + row.toString().count(p => p == Unicode.bytesToChar(replacementChar))
+    })
+    errorCounter = errorCounter - countErrorsInFile()
+    errorCounter / csvFile.length()
   }
 }
