9168a002f5d6e8466a04e3135d09cbf17e70fe0d
Jakob Köhler <jkhlr@users.noreply.github.com>
2019-02-05 17:28:33 +0100

    Split Property (intermediate state) (#25)
    * add SplitProperty
    
    * Finished SplitProperty setup
    
    * add Test
    
    * implemented logic
    
    * fix SplitProperty
    
    * cleanup
    
    * add fromLeft parameter
    
    * implemented method to find times
    
    * implemented method to find times part 2
    
    * implement findSeparator
    
    * refactored methods
    
    * add error handling
    
    * add test
    
    * clean up test
    
    * corrected typo and space.
    
    * added tests with different parameters.
    
    * added additional error handling.
    
    * added additional execption tests.
    
    * make def out of val for splitValue
    
    * restore PaddingTest
    
    * fix test
    
    * added experimental functions for identifying multicharacter seperators
    
    * minor refactorment of try...except logic
    
    * implement calApplicability
    
    * implement tests
    
    * removed lasses experimental code :-(
    
    * add SplitPropertyUtils, add abstract Separator class, restructure execution logic
    
    * implement second evaluation function
    
    * added functionalities for splitting camelcases, currencies
    
    * added functionalities for splitting camelcases, currencies
    
    * Added ClassClass Candidate finder
    
    * Added split and merge vor CharacterCase Seperator (not working yet)
    
    * did stuff.
    
    * added CharacterClassSeparatorNoColNum and instances for findSeperator
    
    * little fix
    
    * fix scoring function
    
    * move tests to scala

---
+++ data-prep/dataprep-simple/src/test/scala/de/hpi/isg/dataprep/preparators/SplitPropertyTest.scala
@@ -0,0 +1,102 @@
+package de.hpi.isg.dataprep.preparators
+
+import de.hpi.isg.dataprep.components.Preparation
+import de.hpi.isg.dataprep.model.target.system.AbstractPreparator
+import de.hpi.isg.dataprep.preparators.define.SplitProperty
+import de.hpi.isg.dataprep.preparators.implementation.DefaultSplitPropertyImpl
+import de.hpi.isg.dataprep.preparators.implementation.DefaultSplitPropertyImpl.SingleValueStringSeparator
+import org.apache.spark.sql.Encoders
+import org.apache.spark.sql.types.{DataTypes, Metadata, StructField, StructType}
+
+class SplitPropertyTest extends PreparatorScalaTest with Serializable {
+  "Column" should "be split correctly if only propertyName is given" in {
+    splitShouldBeCorrect(new SplitProperty("date"))
+  }
+
+  "Column" should "be split correctly if only propertyName and separator are given" in {
+    splitShouldBeCorrect(new SplitProperty("date", SingleValueStringSeparator("-")))
+  }
+
+  "Column" should "be split correctly if all parameters are given" in {
+    splitShouldBeCorrect(new SplitProperty("date", SingleValueStringSeparator("-")))
+  }
+
+  "Column" should "be split from the right" in {
+    val separator = SingleValueStringSeparator("-")
+    val preparator = new SplitProperty("date", separator, 3, false)
+    pipeline.addPreparation(new Preparation(preparator))
+    pipeline.executePipeline()
+
+    pipeline.getErrorRepository.getErrorLogs.size shouldEqual 0
+    pipeline.getRawData.schema shouldEqual newSchema
+
+    pipeline.getRawData.collect().foreach(row => {
+      val expectedSplit = row.get(8).toString.split("-").reverse
+      val split = Array(row.get(12).toString, row.get(13).toString, row.get(14).toString)
+      if (expectedSplit.length == 1)
+        split shouldEqual Array(expectedSplit(0), "", "")
+      else
+        split shouldEqual expectedSplit
+    })
+  }
+
+  "Applicability score" should "be calculated correctly" in {
+    val separator = SingleValueStringSeparator("-")
+    val impl = new DefaultSplitPropertyImpl()
+    val column  = pipeline.getRawData.select("date").as(Encoders.STRING)
+    val score = impl.evaluateSplit(column, separator, 3)
+    score shouldEqual 0.9f
+
+  }
+
+  "IllegalArgumentException" should "be thrown if numCols is 1" in {
+    val separator = SingleValueStringSeparator("-")
+    val preparator = new SplitProperty("data", separator, 1, true)
+    pipeline.addPreparation(new Preparation(preparator))
+    an[IllegalArgumentException] should be thrownBy pipeline.executePipeline()
+  }
+
+  "IllegalArgumentException" should "be thrown if propertyName does not exist in dataset" in {
+    val separator = SingleValueStringSeparator("-")
+    val preparator = new SplitProperty("xyz", separator, 1, true)
+    pipeline.addPreparation(new Preparation(preparator))
+    an[IllegalArgumentException] should be thrownBy pipeline.executePipeline()
+  }
+
+  val newSchema = new StructType(
+    Array[StructField](
+      StructField("id", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("identifier", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("species_id", DataTypes.IntegerType, nullable = true, Metadata.empty),
+      StructField("height", DataTypes.IntegerType, nullable = true, Metadata.empty),
+      StructField("weight", DataTypes.IntegerType, nullable = true, Metadata.empty),
+      StructField("base_experience", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("order", DataTypes.IntegerType, nullable = true, Metadata.empty),
+      StructField("is_default", DataTypes.IntegerType, nullable = true, Metadata.empty),
+      StructField("date", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("stemlemma", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("stemlemma2", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("stemlemma_wrong", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("date1", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("date2", DataTypes.StringType, nullable = true, Metadata.empty),
+      StructField("date3", DataTypes.StringType, nullable = true, Metadata.empty)
+    )
+  )
+
+  def splitShouldBeCorrect(preparator: AbstractPreparator): Unit = {
+    pipeline.addPreparation(new Preparation(preparator))
+    pipeline.executePipeline()
+
+    pipeline.getErrorRepository.getErrorLogs.size shouldEqual 0
+    pipeline.getRawData.schema shouldEqual newSchema
+
+    pipeline.getRawData.collect().foreach(row => {
+      val expectedSplit = row.get(8).toString.split("-")
+      val split = Array(row.get(12).toString, row.get(13).toString, row.get(14).toString)
+      if (expectedSplit.length == 1)
+        split shouldEqual Array(expectedSplit(0), "", "")
+      else
+        split shouldEqual expectedSplit
+    })
+  }
+}
