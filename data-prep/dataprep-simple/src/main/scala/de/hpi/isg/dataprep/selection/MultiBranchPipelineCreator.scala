29c04a2379d65a5993e5a04af84f711f94190547
Nico Duldhardt <nduldhardt@gmail.com>
2019-02-12 14:44:16 +0100

    added pipeline creation tests

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreator.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreator.scala
@@ -1,28 +1,14 @@
 package de.hpi.isg.dataprep.selection
 import de.hpi.isg.dataprep.components.Pipeline
-import de.hpi.isg.dataprep.context.DataContext
+import de.hpi.isg.dataprep.selection.MultiBranchPipelineCreator._
 import org.apache.spark.sql.{DataFrame, Dataset, Row}
+import org.apache.spark.sql.functions.col
 
 import scala.collection.mutable.MutableList
 
 
 class MultiBranchPipelineCreator(dataFrame: DataFrame) {
   val dummyPreparators = List(new DummyPreparator, new DummyPreparator, new DummyPreparator)
-
-  private val MAX_ITERATIONS = 100
-  private val MAX_BRANCHES = 2
-  type Score = Float
-
-  case class Node(
-                           parent: Option[Node],
-                           childs: MutableList[Node],
-                           preparator: Option[DummyPreparator],
-                           affectedCols: Set[String],
-                           dataFrame: DataFrame,
-                           score: Score
-                         )
-
-  case class Candidate(leaf: Node, preparator: DummyPreparator, totalScore: Score)
 
   val root = Node(
     None,
@@ -50,8 +36,8 @@ class MultiBranchPipelineCreator(dataFrame: DataFrame) {
   }
 
 
-  def getBranchAffectedCols(node: Node): Map[DummyPreparator, Set[String]] = {
-    case Node(None, _, _, _, _, _) => Map.empty
+  def getBranchAffectedCols(node: Node): Map[DummyPreparator, Set[String]] = node match {
+    case Node(None, _, _, _, _, _) => Map.empty[DummyPreparator, Set[String]]
     case Node(Some(parent), _, Some(preparator), affectedCols, _, _) => {
       val branchAffectedCols = getBranchAffectedCols(parent)
       val preparatorAffectedCols = branchAffectedCols
@@ -69,11 +55,9 @@ class MultiBranchPipelineCreator(dataFrame: DataFrame) {
 
   // calculates score of the branch up to the (and including) the given node
   def getBranchScore(node: Node): Score = node match {
-      case Node(None, _, _, _, _, _) => 0
-      case Node(Some(parent), _, _, _, _, _) => getBranchScore(parent)
+      case Node(None, _, _, _, _, 0f) => 0f
+      case Node(Some(parent), _, _, _, _, score) => score + getBranchScore(parent)
   }
-
-
 
   def nextCandidates(leaf: Node, preparators: List[DummyPreparator]): List[Candidate]    = {
     val leafBranchScore = getBranchScore(leaf)
@@ -81,11 +65,29 @@ class MultiBranchPipelineCreator(dataFrame: DataFrame) {
       .map(p => Candidate(leaf, p, leafBranchScore + p.calApplicability(leaf.dataFrame)))
   }
 
-  def generateColumnCombinations(preparator: DummyPreparator, df: DataFrame, affectedCols: Set[String]): Set[DataFrame] = {
+  def generateColumnCombinations(preparator: DummyPreparator, df: DataFrame, affectedCols: Set[String]): List[DataFrame] = {
     (df.columns.toSet -- affectedCols)
       .subsets()
-      .map(df.select(_:_*))
-      .toSet
+      .map(_.map(col))
+      .map(cols => df.select(cols.toSeq:_*))
+      .toList
   }
 
+}
+
+object MultiBranchPipelineCreator {
+  private val MAX_ITERATIONS = 100
+  private val MAX_BRANCHES = 2
+  type Score = Float
+
+  case class Node(
+                   parent: Option[Node],
+                   childs: MutableList[Node],
+                   preparator: Option[DummyPreparator],
+                   affectedCols: Set[String],
+                   dataFrame: DataFrame,
+                   score: Score
+                 )
+
+  case class Candidate(leaf: Node, preparator: DummyPreparator, totalScore: Score)
 }
--- data-prep/dataprep-simple/src/test/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreatorTest.scala
+++ data-prep/dataprep-simple/src/test/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreatorTest.scala
@@ -1,13 +1,16 @@
 package de.hpi.isg.dataprep.selection
 
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, Matchers}
+
+import scala.collection.mutable.MutableList
 
 class MultiBranchPipelineCreatorTest extends FlatSpecLike with Matchers with BeforeAndAfterAll {
 
   var preparators: List[DummyPreparator] = _
-
+  val spark: SparkSession = SparkSession.builder().master("local").getOrCreate()
+  var df: DataFrame = _
 
   override def beforeAll(): Unit = {
     val dummyPrep1 = new DummyPreparator("prep1")
@@ -15,15 +18,12 @@ class MultiBranchPipelineCreatorTest extends FlatSpecLike with Matchers with Bef
     val dummyPrep3 = new DummyPreparator("prep3")
 
     preparators = List(dummyPrep1, dummyPrep2, dummyPrep3)
+    import spark.implicits._
+    val testData = List((1,2,3,4))
+    df = testData.toDF("col1", "col2", "col3", "col4")
   }
 
   "MultiBranchPipelineCreator" should "generate correct column combinations" in {
-    val testData = List((1,2,3,4))
-    val spark = SparkSession.builder().master("local").getOrCreate()
-
-    import spark.implicits._
-    val df = testData.toDF("col1", "col2", "col3", "col4")
-
     val dummyPrep = new DummyPreparator("prep")
 
     val pipelineCreator = new MultiBranchPipelineCreator(df)
@@ -33,12 +33,27 @@ class MultiBranchPipelineCreatorTest extends FlatSpecLike with Matchers with Bef
   }
 
   it should "find the affected columns for a branch" in {
+    val root = MultiBranchPipelineCreator.Node(None, MutableList.empty, None, Set.empty, df, 0f)
+    val innerNode1 = MultiBranchPipelineCreator.Node(Some(root), MutableList.empty, Some(preparators(0)), Set("col1"), df, 0.8f)
+    val innerNode2 = MultiBranchPipelineCreator.Node(Some(innerNode1), MutableList.empty, Some(preparators(1)), Set("col4"), df, 0.8f)
+    val leaf = MultiBranchPipelineCreator.Node(Some(innerNode2), MutableList.empty, Some(preparators(0)), Set("col2", "col3"), df, 0.5f)
+    root.childs += innerNode1
+    innerNode1.childs += innerNode2
+    innerNode2.childs += leaf
 
-
+    val pipelineCreator = new MultiBranchPipelineCreator(df)
+    pipelineCreator.getBranchAffectedCols(leaf) should be (Map(preparators(0)->Set("col1", "col2", "col3"),preparators(1)->Set("col4")))
   }
 
   it should "calculate the correct branch score" in {
+    val root = MultiBranchPipelineCreator.Node(None, MutableList.empty, None, Set.empty, df, 0f)
+    val innerNode = MultiBranchPipelineCreator.Node(Some(root), MutableList.empty, Some(preparators(0)), Set.empty, df, 0.8f)
+    val leaf = MultiBranchPipelineCreator.Node(Some(innerNode), MutableList.empty, Some(preparators(1)), Set.empty, df, 0.5f)
+    root.childs += innerNode
+    innerNode.childs += leaf
 
+    val pipelineCreator = new MultiBranchPipelineCreator(df)
+    pipelineCreator.getBranchScore(leaf) should equal (1.3f)
   }
 
 }
