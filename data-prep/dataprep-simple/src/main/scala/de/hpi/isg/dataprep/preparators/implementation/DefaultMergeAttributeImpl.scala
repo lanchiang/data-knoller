cebae6179d4b6b1fcf75f59f6ddb7472bfee1d01
sirexeclp <sirexeclp@gmail.com>
2019-02-15 13:16:37 +0100

    cleanup

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
@@ -11,58 +11,33 @@ import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.apache.spark.util.CollectionAccumulator
 
 class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
-	/**
-	  * The abstract class of preparator implementation.
-	  *
-	  * @param abstractPreparator is the instance of { @link AbstractPreparator}. It needs to be converted to the corresponding subclass in the implementation body.
-	  * @param dataFrame        contains the intermediate dataset
-	  * @param errorAccumulator is the { @link CollectionAccumulator} to store preparation errors while executing the preparator.
-	  * @return an instance of { @link ExecutionContext} that includes the new dataset, and produced errors.
-	  * @throws Exception
-	  */
+
 	override protected def executeLogic(abstractPreparator: AbstractPreparator, dataFrame: Dataset[Row], errorAccumulator: CollectionAccumulator[PreparationError]): ExecutionContext = {
 		val preparator = abstractPreparator.asInstanceOf[MergeAttribute];
-		println(preparator.test)
+		//find a name for the merged column
 		val newColumnName = findName(preparator.attributes(0),preparator.attributes(1))
+		//select the right merge function
 		val mergeFunc = if (preparator.connector.isEmpty) merge else merge(preparator.connector)
+		//merge
 		val df = dataFrame.withColumn(newColumnName, mergeFunc(col(preparator.attributes(0)),col(preparator.attributes(1))))
+		//delete old columns
 		val deleteColumns = preparator.attributes.filter(x => x!= newColumnName)
 		val columns = df.columns.filter( c => !deleteColumns.contains(c)).map(col(_))
-		//findMergeCandidates(dataFrame)
+
 		new ExecutionContext(df.select(columns: _*),errorAccumulator)
 	}
+
 	def merge(connector: String): UserDefinedFunction =
 		udf((col1: String,col2: String) => {
 			col1 + connector + col2
 	})
 
-	def findMergeCandidates( dataset: Dataset[Row]) =
-	{
-		import dataset.sparkSession.implicits._
-		val columns = dataset.columns.toSeq
+	def merge():UserDefinedFunction =
+	udf((col1: String,col2: String) => {
+		if (col1.equals(col2)) col1 else if (col2.trim.nonEmpty) col2 else col1
+	})
 
-		columns.flatMap (
-			col => (
-					columns.filter(x => x != col).map(x=>(col,x))
-					)
-		)
-		.map(x => dataset.select(x._1, x._2))
-		.map(colCombo =>
-					colCombo.map(
-						row =>{
-								val a = row.get(0).toString
-								val b = row.get(1).toString
-								if (a.equals(b))
-									1
-								else if ( a.trim.isEmpty || b.trim.isEmpty)
-									1
-								else
-									0
-					}
-					).reduce(_ + _)
-			).toDF().agg(col("__1"))
 
-	}
 	def isNull(value:String) = value.trim.isEmpty
 	def isGoodToMerge(a:String, b:String):Integer = {
 		if (a.equals(b))
@@ -73,10 +48,6 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 			0
 	}
 
-	def merge():UserDefinedFunction =
-	udf((col1: String,col2: String) => {
-		if (col1.equals(col2)) col1 else if (col2.trim.nonEmpty) col2 else col1
-	})
 
 	def getAllSubstrings(str: String): Set[String] = {
 		str.inits.flatMap(_.tails).toSet
