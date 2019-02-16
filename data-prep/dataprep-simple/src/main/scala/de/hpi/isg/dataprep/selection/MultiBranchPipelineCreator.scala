9f671b8a98f5e1510085b77bc71009014613c5e3
Nico Duldhardt <nduldhardt@gmail.com>
2019-02-16 21:08:14 +0100

    added some comments

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreator.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/selection/MultiBranchPipelineCreator.scala
@@ -7,11 +7,23 @@ import de.hpi.isg.dataprep.selection.MultiBranchPipelineCreator._
 import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.col
 
-
+/**
+  * The MultiBranchPipelineCreator builds a tree of possible Preparation chains and evaluates multiple branches at the same time.
+  * Finally, it returns the best chain of preparations as a pipeline.
+  *
+  * @param dataContext describes the input dataframe
+  */
 class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreator(dataContext) {
+  /**
+    * The root node of the tree.
+    */
   val root: Root = Root(dataContext.getDataFrame)
 
-  // Assembles the pipeline.
+  /**
+    * Assembles a pipeline.
+    *
+    * @return finished Pipeline
+    */
   def createPipeline(): Pipeline = {
     val bestBranch = findBestBranch(root)
     val newPipe = new Pipeline(bestBranch.dataFrame)
@@ -19,13 +31,25 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
     newPipe
   }
 
+  /**
+    * Helper method to assemble a pipeline.
+    *
+    * @param treeNode branch head that identifies the best branch that will be transformed to a pipeline.
+    * @return list of preparations in the best tree branch.
+    */
   def addToPipe(treeNode: TreeNode): List[Preparation] = treeNode match {
     case Root(_) => Nil
     case Child(parent, preparator, _, _, _) => addToPipe(parent) :+ new Preparation(preparator)
   }
 
 
-  // Returns the leaf node that represents the branch with the highest total score.
+  /**
+    * Given the root, a tree is iteratively built. The tree is pruned and stop conditions are evaluated in between.
+    * Returns the leaf node that represents the branch with the highest total score.
+    *
+    * @param root root node of the tree
+    * @return leaf with the highest total branch score
+    */
   def findBestBranch(root: Root): Child = {
     def rec(branchHeads: List[TreeNode], maxIterations: Int): List[TreeNode] = {
       if (maxIterations == 0) return branchHeads
@@ -56,6 +80,13 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
   }
 
   // Transforms a candidate to a tree node to append it to the graph.
+  /**
+    * Transforms a candidate to a tree node and appends it to the tree. Executes the candidates preparator to generate the
+    * new intermediate dataset.
+    *
+    * @param candidate candidate that should be evaluated.
+    * @return child node connected with the tree
+    */
   def candidateToBranchHead(candidate: Candidate): Child = {
     val oldBranchHead = candidate.branchHead
     val preparator = candidate.preparator
@@ -68,7 +99,14 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
     Child(oldBranchHead, preparator, colComb, newDf, prepScore)
   }
 
-  // Returns a map from all preparators in a branch to all columns they affected.
+  /**
+    * Returns a map from all preparator classes in a branch to all columns they affected.
+    * This allows us to enforce that a preparator can only affect a column once and stop the tree
+    * growing process accordingly.
+    *
+    * @param node node that represents a tree branch
+    * @return map from preparator classes to their affected column set
+    */
   def getBranchAffectedCols(node: TreeNode): Map[Class[_ <: AbstractPreparator], Set[String]] = node match {
     case Root(_) => Map.empty[Class[_ <: AbstractPreparator], Set[String]]
     case Child(parent, preparator, affectedCols, _, _) => {
@@ -81,6 +119,14 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
     }
   }
 
+  /**
+    * We pass a list of candidates and candidates that refer to equivalent branches get thrown away. An equivalent branch
+    * is a branch that leads to the same intermediate dataset despite having a different operator order.
+    * Preparators that affect different columns don't interfere with each other and can be applied in any order.
+    *
+    * @param candidates candidates to be pruned.
+    * @return pruned candidates
+    */
   def pruneEquivalentBranches(candidates: List[Candidate]): List[Candidate] = {
     def getBranchStructure(branchHead: TreeNode): List[(String, Set[String])] = branchHead match {
       case Root(_) => Nil
@@ -107,13 +153,22 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
     distinctCandidates
   }
 
-  // This dummy functions just show how schema and metadata similarity could be applied to modify the selected preparators
+  /**
+    * This dummy functions just shows how schema and metadata similarity could be applied to modify the selected preparators.
+    * We can weigh the scores with the similarity as shown below.
+    *
+    */
   def calculateSchemaSimilarity = 1
-
   def calculateMetaDataSimilarity = 1
 
 
-  // Filters the k best candidates from all potential candidates to extend the tree.
+  /**
+    * Filters the k best candidates from all potential candidates to extend the tree.
+    *
+    * @param k The number of candidates that should be kept.
+    * @param candidates Original candidates
+    * @return k best candidates
+    */
   def kBestCandidates(k: Int, candidates: List[Candidate]): List[Candidate] = {
     if (k == 0) Nil
     else if (candidates.size <= k) candidates
@@ -126,15 +181,26 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
     }
   }
 
-  // calculates score of the branch up to the (and including) the given node
+  /**
+    * Calculates score of the branch up to the (and including) the given node.
+    *
+    * @param node that represents the tree branch
+    * @return total score of the branch
+    */
   def getBranchScore(node: TreeNode): Score = node match {
     case Root(_) => 0f
     case Child(parent, _, _, _, score) => score + getBranchScore(parent)
   }
 
-  // Returns the candidates to extend the tree branch represented by the branchHead. The List
-  // of preparators represents all possible preparation operators.
-  // The preparator applicabilities are calculated for all valid column combinations for each preparator.
+  /**
+    * Generates candidates to extend the current tree by a layer. Each candidate represents a preparator applied to a
+    * the intermediate dataset of the previous layer. The list of preparator classes allow us to instantiate the different preparators
+    * and calculate their applicability scores.
+    *
+    * @param branchHead branchHead that could be extended by the generated candidates
+    * @param preparators all possible preparators
+    * @return candidates to extend the branch
+    */
   def nextCandidates(branchHead: TreeNode, preparators: List[Class[_ <: AbstractPreparator]]): List[Candidate] = {
     val branchScore = getBranchScore(branchHead)
     val affectedCols = getBranchAffectedCols(branchHead)
@@ -148,6 +214,13 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
 
   // This generates all the column combinations and allows to exclude certain columns.
   // This allows us to prevent a preparator from affecting a column twice.
+  /**
+    * Generate the column combinations and extend certain columns.
+    *
+    * @param df original dataframe
+    * @param excludeCols excluded columns
+    * @return the generated column combinations
+    */
   def generateColumnCombinations(df: DataFrame, excludeCols: Set[String]): List[DataFrame] = {
     (df.columns.toSet -- excludeCols)
       .subsets()
@@ -160,11 +233,25 @@ class MultiBranchPipelineCreator(dataContext: DataContext) extends PipelineCreat
 }
 
 object MultiBranchPipelineCreator {
+  /**
+    * Maximal tree depth.
+    */
   private val MAX_ITERATIONS = 100
+  /**
+    * Maximal number of branches to be kept. A higher number increases the performance but also leads to a higher runtime.
+    */
   private val MAX_BRANCHES = 2
+  /**
+    * This threshold filters preparators with a lower applicability score.
+    * We usually do not want preparators with a low score to be applied.
+    */
   private val PREPARATOR_SCORE_THRESHOLD = 0.5
+
   type Score = Float
 
+  /**
+    * Tree datastructure
+    */
   sealed trait TreeNode {
     def dataFrame: DataFrame
   }
@@ -178,6 +265,9 @@ object MultiBranchPipelineCreator {
                    score: Score
                   ) extends TreeNode
 
+  /**
+    * Candidates to possibly extend the tree.
+    */
   case class Candidate(branchHead: TreeNode, preparator: AbstractPreparator, affectedCols: Set[String], totalScore: Score, score: Score)
 
 }
