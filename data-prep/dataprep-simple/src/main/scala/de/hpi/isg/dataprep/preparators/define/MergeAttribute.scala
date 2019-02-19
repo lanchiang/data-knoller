bb29363e4bf60defd213eb5a0c1fbb9357ea44ff
sirexeclp <sirexeclp@gmail.com>
2019-02-19 12:43:41 +0100

    cleanup and comments

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/MergeAttribute.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/define/MergeAttribute.scala
@@ -10,192 +10,252 @@ import org.apache.spark.sql.functions.udf
 import spire.std.float
 
 import scala.collection.JavaConverters._
-class MergeAttribute (var attributes:List[String]
-					 ,var connector:String
-					 ,val handleMergeConflict:(String,String) => String = null)
-		extends AbstractPreparator{
-	def this()
-	{
-		this(List[String](),"")
-	}
 
-	def this(attributes: java.util.List[String], connector:String)
-	{
-		this((attributes.asScala.toList), connector,null)
-	}
+class MergeAttribute(var attributes: List[String]
+					 , var connector: String
+					 , val handleMergeConflict: (String, String) => String = null)
+		extends AbstractPreparator {
 
-	//def this(attributes: java.util.List[Integer], connector: String)
-	//{
-	//	this(Right(attributes.asScala.toList), connector)
-	//}
-
-
-	def mapMerge(row: Row) = MergeUtil.isGoodToMerge(row.getString(0),row.getString(1))
-
-
-  def headerIsYear(header: String): Boolean = if (header.contains("Year") || header.contains("year")) true else false
-
-  def headerIsMonth(header: String): Boolean = if (header.contains("Month") || header.contains("month")) true else false
-
-  def headerIsDay(header: String): Boolean = if (header.contains("Day") || header.contains("day")) true else false
-
-
-  def isYear(row: Dataset[Row], header: String): Boolean = {
-    import row.sparkSession.implicits._
-
-    val columnWithoutNull = row
-      .select(header)
-      .filter(x => x.get(0).toString.trim.isEmpty)
-
-    val sumYear = columnWithoutNull
-      .map(x => if (x.getInt(0) <= 2100 && x.getInt(0) > 0 ) 1 else 0)
-      .reduce(_+_)
-
-    if (sumYear / columnWithoutNull.count() >= 1)
-      true
-    else
-      false
-  }
-
-  def isMonth(row: Dataset[Row], header: String): Boolean = {
-		import row.sparkSession.implicits._
-
-		val columnWithoutNull = row
-			.select(header)
-			.filter(x => x.get(0).toString.trim.isEmpty)
-
-		val sumMonth = columnWithoutNull
-			.map(x => if (x.getInt(0) <= 12 && x.getInt(0) > 0 ) 1 else 0)
-			.reduce(_+_)
-
-    if (sumMonth / columnWithoutNull.count() >= 0.95)
-      true
-    else
-      false
-  }
-
-  def isDay(row: Dataset[Row],header: String): Boolean = {
-  import row.sparkSession.implicits._
-
-    val columnWithoutNull = row
-        .select(header)
-        .filter(x => x.get(0).toString.trim.isEmpty)
-
-    val sumDay = columnWithoutNull
-			.map(x => if (x.getInt(0) <= 31 && x.getInt(0) > 0 ) 1 else 0)
-			.reduce(_+_)
-
-    if (sumDay / columnWithoutNull.count() >= 0.95)
-      true
-    else
-      false
-  }
-
-//	def ksTest(dataset: Dataset[Row]):Double = {
-//		dataset.sort("col1","col2")
-//
-//
-//
-//	}
-
-  override def buildMetadataSetup(): Unit = {
-
-	}
-
+	/***  THRESHOLDS FOR calApplicability  ***/
 	val weight = 4
 	val bias = -0.75
 
-	def callApplicabilityDate(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]) :Float={
-	import dataset.sparkSession.implicits._
-
-    val dateCombi =  dataset.columns.seq.flatMap( x=>
-      dataset.columns.map(y=>(y,x))
-    ).filter(x=> x._1 != x._2)
-      .flatMap( x=>
-        dataset.columns.map(y=>(y,x._1,x._2))
-      ).filter(x=> x._3 != x._2)
-      .filter(x=> x._3 != x._1)
-      .filter(x => headerIsDay(x._1) && headerIsMonth(x._2) && headerIsYear(x._3))
 
 
-    if (dateCombi.seq.length>0){
-      	val result = dateCombi.seq(0)
-      	this.attributes = List(result._1,result._2,result._3)
-		this.connector = "."
-		1
-    }
-	else 0
+	/** * CTORS ***/
+	def this() {
+		this(List[String](), "")
 	}
 
+	def this(attributes: java.util.List[String], connector: String) {
+		this((attributes.asScala.toList), connector, null)
+	}
+
+
+	//def mapMerge(row: Row) = MergeUtil.isGoodToMerge(row.getString(0),row.getString(1))
+
+	/** * DATE HANDLING ***/
+
+	/***
+	  * Checks if header to find year column.
+	  * @param header column name
+	  * @return true if column is year
+	  */
+	def headerIsYear(header: String): Boolean = if (header.toLowerCase.contains("year")) true else false
+	/***
+	  * Checks if header to find month column.
+	  * @param header column name
+	  * @return true if column is month
+	  */
+	def headerIsMonth(header: String): Boolean = if (header.toLowerCase.contains("month")) true else false
+	/***
+	  * Checks if header to find day column.
+	  * @param header column name
+	  * @return true if column is day
+	  */
+	def headerIsDay(header: String): Boolean = if (header.toLowerCase.contains("day")) true else false
+
+	/***
+	  * Check column contents to check if it contains the year part of a date.
+	  * @param dataset dataset to find year in
+	  * @param header name of column to inspect
+	  * @return true if column is year
+	  */
+	def isYear(dataset: Dataset[Row], header: String): Boolean = {
+		import dataset.sparkSession.implicits._
+
+		val columnWithoutNull = dataset
+				.select(header)
+				.filter(x => MergeUtil.isNull(x.get(0).toString))
+
+		val sumYear = columnWithoutNull
+				.map(x => if (x.getInt(0) <= 2100 && x.getInt(0) > 0) 1 else 0)
+				.reduce(_ + _)
+
+		if (sumYear / columnWithoutNull.count() >= 1)
+			true
+		else
+			false
+	}
+
+	/***
+	  * Check column contents to check if it contains the month part of a date.
+	  * @param dataset dataset to find month in
+	  * @param header name of column to inspect
+	  * @return true if column is month
+	  */
+	def isMonth(dataset: Dataset[Row], header: String): Boolean = {
+		import dataset.sparkSession.implicits._
+
+		val columnWithoutNull = dataset
+				.select(header)
+				.filter(x => x.get(0).toString.trim.isEmpty)
+
+		val sumMonth = columnWithoutNull
+				.map(x => if (x.getInt(0) <= 12 && x.getInt(0) > 0) 1 else 0)
+				.reduce(_ + _)
+
+		if (sumMonth / columnWithoutNull.count() >= 0.95)
+			true
+		else
+			false
+	}
+
+	/***
+	  * Check column contents to check if it contains the day part of a date.
+	  * @param dataset dataset to find day in
+	  * @param header name of column to inspect
+	  * @return true if column is day
+	  */
+	def isDay(dataset: Dataset[Row], header: String): Boolean = {
+		import dataset.sparkSession.implicits._
+
+		val columnWithoutNull = dataset
+				.select(header)
+				.filter(x => x.get(0).toString.trim.isEmpty)
+
+		val sumDay = columnWithoutNull
+				.map(x => if (x.getInt(0) <= 31 && x.getInt(0) > 0) 1 else 0)
+				.reduce(_ + _)
+
+		if (sumDay / columnWithoutNull.count() >= 0.95)
+			true
+		else
+			false
+	}
+	
+	override def buildMetadataSetup(): Unit = {
+
+	}
+
+	/***
+	  * This function calculates the callApplicability assuming we want to merge date parts (year, month, day).
+	  *
+	  * @param schemaMapping is the schema of the input data towards the schema of the output data.
+	  * @param dataset is the input dataset slice. A slice can be a subset of the columns of the data,
+	  *                or a subset of the rows of the data.
+	  * @param targetMetadata is the set of {@link Metadata} that shall be fulfilled for the output data
+	  * @return 0 if list of rows is not (day, month, year) or more or less than 3 columns are given. returns 1 if date if found
+	  */
+	def callApplicabilityDate(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]): Float = {
+		import dataset.sparkSession.implicits._
+		val columns = dataset.columns.toSeq
+		if (columns.length != 3)
+			return 0
+		if(headerIsDay(columns(0)) && headerIsMonth(columns(1)) && headerIsYear(columns(2)))
+		{
+			this.attributes = columns.toList
+			this.connector = "."
+			return 1
+		}
+		else
+			return 0
+	}
+
+	/***
+	  * Calculates calApplicability for columns to merge.
+	  * Expects 2 columns for normal merge and 3 columns for date-parts-merge.
+	  * If more than 3 columns are given, returns 0.
+	  * @param schemaMapping is the schema of the input data towards the schema of the output data.
+	  * @param dataset is the input dataset slice. A slice can be a subset of the columns of the data,
+	  *                or a subset of the rows of the data.
+	  * @param targetMetadata is the set of {@link Metadata} that shall be fulfilled for the output data
+	  * 	  */
 	override def calApplicability(schemaMapping: SchemaMapping, dataset: Dataset[Row], targetMetadata: util.Collection[Metadata]): Float = {
 		import dataset.sparkSession.implicits._
 		val columns = dataset.columns.toSeq
-		if(columns.length == 3)
-			return callApplicabilityDate(schemaMapping,dataset,targetMetadata)
+		if (columns.length == 3)
+			return callApplicabilityDate(schemaMapping, dataset, targetMetadata)
 		if (columns.length != 2)
 			return 0
 
 		val applicability = dataset
-				.map( x => {
+				.map(x => {
 					val a = if (x.isNullAt(0)) null else x.get(0).toString
 					val b = if (x.isNullAt(1)) null else x.get(1).toString
-					MergeUtil.isGoodToMerge(a,b) //calculate mergeGoodness for each row
+					MergeUtil.isGoodToMerge(a, b) //calculate mergeGoodness for each row
 				})
-				.reduce(_+_) / dataset.count().toFloat
+				.reduce(_ + _) / dataset.count().toFloat
 		//apply threshold
-		val result = (Math.max(applicability + bias,0) * weight).toFloat
+		val result = (Math.max(applicability + bias, 0) * weight).toFloat
 		//remember columns for later merge operation
 		this.attributes = columns.toList
-//		chi2Test(dataset)
+		//		chi2Test(dataset)
 		//return weighted mergeGoodness
 		result
 	}
-	def chi2Test(dataset: Dataset[Row])={
+
+	def chi2Test(dataset: Dataset[Row]) = {
 		import dataset.sparkSession.implicits._
-	//	if dataset.columns.length != 2 throw Exception("Bla")
-//		val colA = dataset.select(dataset.columns(0))
-//		val colB = dataset.select(dataset.columns(1))
-//		val nA = colA.groupBy(colA.columns(0)).count()
-//		val nB = colB.groupBy(colB.columns(0)).count()
+		//	if dataset.columns.length != 2 throw Exception("Bla")
+		//		val colA = dataset.select(dataset.columns(0))
+		//		val colB = dataset.select(dataset.columns(1))
+		//		val nA = colA.groupBy(colA.columns(0)).count()
+		//		val nB = colB.groupBy(colB.columns(0)).count()
 
 		dataset
-			.groupBy(dataset.columns(0),dataset.columns(1))
-			.count()
-			.show()
+				.groupBy(dataset.columns(0), dataset.columns(1))
+				.count()
+				.show()
 
-//		val countA = nA.map(_.getInt(0)).reduce(_+_)
-//		val countB = nB.map(_.getInt(0)).reduce(_+_)
-//
-//		val n = countA + countB
-//
-//		val Ea = nA
-//        		.map(x => countA * x.getInt(0) / n)
-//		val Eb = nB
-//				.map(x => countB * x.getInt(0) / n)
-//
-//		val chi2 = nA.
+		//		val countA = nA.map(_.getInt(0)).reduce(_+_)
+		//		val countB = nB.map(_.getInt(0)).reduce(_+_)
+		//
+		//		val n = countA + countB
+		//
+		//		val Ea = nA
+		//        		.map(x => countA * x.getInt(0) / n)
+		//		val Eb = nB
+		//				.map(x => countB * x.getInt(0) / n)
+		//
+		//		val chi2 = nA.
 
 	}
 }
 
-object MergeUtil extends Serializable{
-	def isNull(value:String):Boolean = {
+/***
+  * This object implements some helper methods used for merging.
+  */
+object MergeUtil extends Serializable {
+
+	/***
+	  * Checks for null values in string. eg. null (nullptr), "null"
+	  * @param value string to check for null
+	  * @return true if string is a null value
+	  */
+	def isNull(value: String): Boolean = {
 		if (value == null)
 			true
- 		else if (value.toLowerCase == "null")
+		else if (value.toLowerCase == "null")
 			true
 		else
 			value.trim.isEmpty
 	}
-	def isGoodToMerge(a:String, b:String):Integer = {
-		if ( isNull(a) || isNull(b))
+
+	/***
+	  * This function calculates if string a and b can be merged, without causing a conflict.
+	  * @param a string to merge
+	  * @param b an other string to merge
+	  * @return true if merge would not generate a conflict
+	  */
+	def isGoodToMerge(a: String, b: String): Integer = {
+		if (isNull(a) || isNull(b))
 			1
 		else if (a.equals(b))
 			1
 		else
 			0
 	}
-	def handleMergeConflicts(a:String,b:String) = {
+
+	/***
+	  * Default implementation of merge conflict handler.
+	  * This function is called, when two values can not be merged.
+	  * To resolve the conflict, the two values are concatenated with a whitespace between them.
+	  * @param a string to merge
+	  * @param b an other string to merge
+	  * @return merged string of a and b
+	  */
+	def handleMergeConflicts(a: String, b: String) = {
 		a + " " + b
 	}
 }
--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
@@ -12,23 +12,37 @@ import org.apache.spark.util.CollectionAccumulator
 
 class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 
+	/***
+	  * Recursively merges columns. Selects column-names, merge function, conflict resolution function.
+	  * @param abstractPreparator is the instance of {@link AbstractPreparator}. It needs to be converted to the corresponding subclass in the implementation body.
+	  * @param dataFrame          contains the intermediate dataset
+	  * @param errorAccumulator   is the {@link CollectionAccumulator} to store preparation errors while executing the preparator.
+	  * @return an instance of {@link ExecutionContext} that includes the new dataset, and produced errors.
+	  */
 	override protected def executeLogic(abstractPreparator: AbstractPreparator, dataFrame: Dataset[Row], errorAccumulator: CollectionAccumulator[PreparationError]): ExecutionContext = {
 		val preparator = abstractPreparator.asInstanceOf[MergeAttribute];
+
 		//find a name for the merged column
 		val newColumnName = findName(preparator.attributes(0),preparator.attributes(1))
 
 		//find conflict resolution function
 		val conflictFunc = if (preparator.handleMergeConflict != null) preparator.handleMergeConflict else MergeUtil.handleMergeConflicts _
+
 		//select the right merge function
 		val mergeFunc = if (MergeUtil.isNull(preparator.connector)) merge(conflictFunc) else merge(preparator.connector)
 
 		//merge
 		val df = dataFrame.withColumn(newColumnName, mergeFunc(col(preparator.attributes(0)),col(preparator.attributes(1))))
+
 		//delete old columns
-		val deleteColumns = preparator.attributes.splitAt(2)._1.filter(x => x!= newColumnName)
-		val columns = df.columns.filter( c => !deleteColumns.contains(c)).map(col(_))
+
+		//only remove merged columns (we only looked at the first 2 in the list)
+		//make a list of columns that should be deleted
+		val deleteColumns = preparator.attributes.splitAt(2)._1 filterNot List(newColumnName).contains
+		val columns = (df.columns diff deleteColumns).map(col(_))
 
 		val result = df.select(columns: _*)
+		//merge recursively if more than 2 columns are given
 		if(preparator.attributes.length>2)
 		{
 			preparator.attributes = newColumnName :: preparator.attributes.splitAt(2)._2
@@ -36,6 +50,16 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 		}
 		else new ExecutionContext(result,errorAccumulator)
 	}
+
+	/***
+	  * This spark UDF merges two given columns by concatenating them.
+	  * If it encounters null values it only returns the other value.
+	  * If both values are null it returns null.
+	  * @param connector string to connect two values
+	  * @return concatenation of given columns with connector
+	  *
+	  *         col1 + connector + col2
+	  */
 
 	def merge(connector: String): UserDefinedFunction =
 		udf((col1: String,col2: String) => {
@@ -49,14 +73,36 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 				col1 + connector + col2
 	})
 
+	/***
+	  * This spark UDF merges two given columns by selecting one of the values.
+	  * Values are merged if they are equal or one of them is null.
+	  * A merge conflict exists when the values are not equal.
+	  * To resolve the merge conflict the handleConflicts function is called.
+	  * The handleConflicts function can be defined when creating the preparator.
+	  * If not handleConflicts function is specified, the default implementation MergeUtil.handleMergeConflicts is chosen.
+	  * @param handleConflicts the handleConflicts function is called to resolve merge conflicts
+	  * @return merged column
+	  */
 	def merge(handleConflicts:(String,String)=>String):UserDefinedFunction =
 	udf((col1: String,col2: String) => {
 		if (MergeUtil.isNull(col1)) col2 else if (MergeUtil.isNull(col2)) col1 else if (col1.equals(col2)) col1  else handleConflicts(col1,col2)
 	})
 
+	/**
+	  * calculates all possible substrings of the given string
+	  * @param str input string
+	  * @return list of substrings
+	  */
 	def getAllSubstrings(str: String): Set[String] = {
 		str.inits.flatMap(_.tails).toSet
 	}
+
+	/***
+	  * Calculates the longest common substring for two given strings.
+	  * @param str1 first string
+	  * @param str2	second string
+	  * @return longest substring
+	  */
 	def longestCommonSubstring(str1: String, str2: String): String = {
 		val str1Substrings = getAllSubstrings(str1)
 		val str2Substrings = getAllSubstrings(str2)
@@ -64,6 +110,12 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 		str1Substrings.intersect(str2Substrings).maxBy(_.length)
 	}
 
+	/***
+	  * findName finds a name for the new column that contains the merge result.
+	  * @param a name of the fist input column
+	  * @param b name of the second input column
+	  * @return name of the resulting merged column
+	  */
 	def findName(a:String, b:String) = {
 		val lcs =  longestCommonSubstring(a,b)
 		//TODO relative on string length?
