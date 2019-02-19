e9241d895424ac77592109e841fa4778fd0b69ab
sirexeclp <sirexeclp@gmail.com>
2019-02-19 11:41:56 +0100

    handle nulls when concat merging + call merge recursively if more than 2 columns are given

--- data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
+++ data-prep/dataprep-simple/src/main/scala/de/hpi/isg/dataprep/preparators/implementation/DefaultMergeAttributeImpl.scala
@@ -20,30 +20,38 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 		//find conflict resolution function
 		val conflictFunc = if (preparator.handleMergeConflict != null) preparator.handleMergeConflict else MergeUtil.handleMergeConflicts _
 		//select the right merge function
-		val mergeFunc = if (preparator.mergeDate) mergeDate else if (preparator.connector.isEmpty) merge(conflictFunc) else merge(preparator.connector)
+		val mergeFunc = if (MergeUtil.isNull(preparator.connector)) merge(conflictFunc) else merge(preparator.connector)
 
 		//merge
 		val df = dataFrame.withColumn(newColumnName, mergeFunc(col(preparator.attributes(0)),col(preparator.attributes(1))))
 		//delete old columns
-		val deleteColumns = preparator.attributes.filter(x => x!= newColumnName)
+		val deleteColumns = preparator.attributes.splitAt(2)._1.filter(x => x!= newColumnName)
 		val columns = df.columns.filter( c => !deleteColumns.contains(c)).map(col(_))
 
-		new ExecutionContext(df.select(columns: _*),errorAccumulator)
+		val result = df.select(columns: _*)
+		if(preparator.attributes.length>2)
+		{
+			preparator.attributes = newColumnName :: preparator.attributes.splitAt(2)._2
+			executeLogic(preparator,result,errorAccumulator)
+		}
+		else new ExecutionContext(result,errorAccumulator)
 	}
-
-	def mergeDate(): UserDefinedFunction =
-		udf((col1: String,col2: String,col3: String) => {
-			col1 + "." + col2 + "." +col3
-		})
 
 	def merge(connector: String): UserDefinedFunction =
 		udf((col1: String,col2: String) => {
-			col1 + connector + col2
+			if(MergeUtil.isNull(col1) && MergeUtil.isNull(col2))
+				null
+			else if(MergeUtil.isNull(col1))
+				col2
+			else if (MergeUtil.isNull(col2))
+				col1
+			else
+				col1 + connector + col2
 	})
 
 	def merge(handleConflicts:(String,String)=>String):UserDefinedFunction =
 	udf((col1: String,col2: String) => {
-		if (col1.equals(col2)) col1 else if (MergeUtil.isNull(col1)) col2 else if (MergeUtil.isNull(col2)) col1 else handleConflicts(col1,col2)
+		if (MergeUtil.isNull(col1)) col2 else if (MergeUtil.isNull(col2)) col1 else if (col1.equals(col2)) col1  else handleConflicts(col1,col2)
 	})
 
 	def getAllSubstrings(str: String): Set[String] = {
@@ -55,8 +63,6 @@ class DefaultMergeAttributeImpl extends  AbstractPreparatorImpl{
 
 		str1Substrings.intersect(str2Substrings).maxBy(_.length)
 	}
-
-
 
 	def findName(a:String, b:String) = {
 		val lcs =  longestCommonSubstring(a,b)
