a2286f2e6650a9ebb5ece8c47919c6e50f76de36
Lan Jiang <jianglan1350@gmail.com>
2019-03-07 16:11:23 +0100

    disallow spark table display in unit tests

--- data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/PipelineTest.java
+++ data-prep/dataprep-simple/src/test/java/de/hpi/isg/dataprep/preparators/PipelineTest.java
@@ -65,8 +65,6 @@ public class PipelineTest extends PreparatorTest {
 
     @Test
     public void testPipelineOnPokemon() throws Exception {
-        pipeline.getRawData().show();
-        pipeline.getRawData().printSchema();
 
         AbstractPreparator prep1 = new ReplaceSubstring("identifier", "[(\\s)+]", "");
         AbstractPreparation preparation1 = new Preparation(prep1);
